12a29b58285c2ccbd03d572c78414ab4a775683f
MiguelMartinez94 <122044737@upq.edu.mx>
2026-03-10 17:55:43 -0600

    Ajustes

--- Cliente_Flask/app/app.py
+++ Cliente_Flask/app/app.py
@@ -1,10 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+@app.route('/')
 def home():
     return "Hola Flask"
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/ventas')
+def ventas():
+    return render_template('pagina_ventas.html')
+
+@app.route('/carrito')
+def carrito():
+    return render_template('carrito_ventas.html')
+
+@app.route('/historial')
+def historial():
+    return render_template('historial_compras.html')
+
+@app.route('/pago')
+def pago():
+    return render_template('pago_exitoso.html')
+
 
 if __name__ == "__main__":
     app.run()
